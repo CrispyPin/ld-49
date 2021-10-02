52bd4fd5e6f631b44c35d61b7e6c77f9bbf50ea1
ErikBot42 <erik.arn.magnusson@gmail.com>
2021-10-02 15:41:24 +0200

    move files, camera

---
+++ src/scripts/Camera.gd
@@ -0,0 +1,20 @@
+extends Camera
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+onready var player = get_node("/root/Game/Player")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	translation = player.translation + Vector3(0,2,5)
+	rotation_degrees = Vector3(-30,0,0)
+	pass
+    
