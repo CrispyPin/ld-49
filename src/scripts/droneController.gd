d80a21b7e9ca73b0eb9c01a866441f601a6d9dae
ErikBot42 <erik.arn.magnusson@gmail.com>
2021-10-02 10:19:39 +0200

    droneController.gd

--- src/scripts/droneController.gd
+++ src/scripts/droneController.gd
@@ -1,4 +1,4 @@
-extends Node
+extends RigidBody
 
 
 # Declare member variables here. Examples:
@@ -8,9 +8,23 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
 
+	pass # Replace with function body.
+
+var yForce = 9.8
+var throttleSpeed = 1
+func _integrate_forces(state):
+	state.set_angular_velocity(Vector3(0,yForce,0))
+
+func _physics_process(delta):
+	var decreaseForceFactor = translation.y*0.8
+	add_central_force(Vector3(0,yForce-decreaseForceFactor,0))
+	#rotation_degrees.x = yForce;
+	#rotate_y(delta*yForce)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
+func _process(delta):
+	if Input.is_action_pressed("decrease_throttle"):
+		yForce-=throttleSpeed;
+	if Input.is_action_pressed("increase_throttle"):
+		yForce+=throttleSpeed;
