58bc184e400b4c9a10625641cb3790bc651a7b8d
ErikBot42 <erik.arn.magnusson@gmail.com>
2021-10-02 11:25:16 +0200

    drone controller proper turn

--- src/scripts/droneController.gd
+++ src/scripts/droneController.gd
@@ -11,21 +11,34 @@ func _ready():
 
 	pass # Replace with function body.
 
-var yForce = 9.8
+var yAcceleration = 9.8
 var throttleSpeed = 0.5
 
 var xdir = 0; # left right 
 var zdir = 0; # forward backward
 
+
+
+
 func _integrate_forces(state):
-	state.set_angular_velocity(Vector3(0,yForce,0))
+	#state.set_angular_velocity(Vector3(0,yAcceleration,0))
+	state.transform = transform.looking_at(translation + Vector3(0,zdir,-1),Vector3(xdir,1,zdir))
+	pass
 
 func _physics_process(delta):
-	var decreaseForceFactor = translation.y*0.8
-	var totalForce = 60*(yForce-decreaseForceFactor)
-	add_central_force(delta*Vector3(xdir*100,totalForce,zdir*100))
-	#rotation_degrees.x = yForce;
-	#rotate_y(delta*yForce)
+	#look_at(translation + Vector3(0,zdir,-1),Vector3(xdir,1,zdir))
+	var decreaseAccelerationFactor = translation.y*0.4
+	var totalYAcceleration = (yAcceleration-decreaseAccelerationFactor)
+
+	var up = transform.basis.y
+
+	var xAcceleration = up.x*yAcceleration;
+	var zAcceleration = up.z*yAcceleration;
+
+	add_central_force(mass*Vector3(xAcceleration,totalYAcceleration,zAcceleration))
+	#rotation_degrees.x = yAcceleration;
+	#rotate_y(delta*yAcceleration)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -34,7 +47,7 @@ func _process(delta):
 		zdir = -1.0
 	elif Input.is_action_pressed("backward"):
 		zdir = 1.0
-	
+
 	xdir = 0
 	if Input.is_action_pressed("left"):
 		xdir = -1.0	
@@ -42,6 +55,9 @@ func _process(delta):
 		xdir = 1.0
 
 	if Input.is_action_pressed("decrease_throttle"):
-		yForce-=throttleSpeed;
+		yAcceleration-=throttleSpeed
 	if Input.is_action_pressed("increase_throttle"):
-		yForce+=throttleSpeed;
+		yAcceleration+=throttleSpeed
+
+
+	
