331e2f80757a65767f583c3f7b585ab6d3cbc27a
ErikBot42 <erik.arn.magnusson@gmail.com>
2021-10-02 10:41:46 +0200

    xy dir

--- src/scripts/droneController.gd
+++ src/scripts/droneController.gd
@@ -12,18 +12,35 @@ func _ready():
 	pass # Replace with function body.
 
 var yForce = 9.8
-var throttleSpeed = 1
+var throttleSpeed = 0.5
+
+var xdir = 0; # left right 
+var zdir = 0; # forward backward
+
 func _integrate_forces(state):
 	state.set_angular_velocity(Vector3(0,yForce,0))
 
 func _physics_process(delta):
 	var decreaseForceFactor = translation.y*0.8
-	add_central_force(Vector3(0,yForce-decreaseForceFactor,0))
+	var totalForce = 60*(yForce-decreaseForceFactor)
+	add_central_force(delta*Vector3(xdir*100,totalForce,zdir*100))
 	#rotation_degrees.x = yForce;
 	#rotate_y(delta*yForce)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	zdir = 0
+	if Input.is_action_pressed("forward"):
+		zdir = -1.0
+	elif Input.is_action_pressed("backward"):
+		zdir = 1.0
+	
+	xdir = 0
+	if Input.is_action_pressed("left"):
+		xdir = -1.0	
+	elif Input.is_action_pressed("right"):
+		xdir = 1.0
+
 	if Input.is_action_pressed("decrease_throttle"):
 		yForce-=throttleSpeed;
 	if Input.is_action_pressed("increase_throttle"):
