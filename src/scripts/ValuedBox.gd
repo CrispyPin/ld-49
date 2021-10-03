6a434dc1df11c92d7636673eab8b3eeef7555ba0
CrispyPin <crispin@tasa.se>
2021-10-03 13:25:04 +0200

    banana for scale, randomise packages

---
+++ src/scripts/ValuedBox.gd
@@ -0,0 +1,16 @@
+extends RigidBody
+
+
+const package_models = [
+	preload("res://models/packages/package-1.glb"),
+	preload("res://models/packages/package-2.glb"),
+	preload("res://models/packages/package-3.glb"),
+]
+
+var type: int
+
+func _ready() -> void:
+	$MeshInstance.queue_free()
+	type = randi() % len(package_models)
+	var model = package_models[type].instance()
+	add_child(model)
