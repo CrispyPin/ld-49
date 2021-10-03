b2b4325224f5aa85598d6737e4297851523679e8
ErikBot42 <erik.arn.magnusson@gmail.com>
2021-10-03 13:27:31 +0200

    tile manager delete objects

--- src/scripts/tile_manager.gd
+++ src/scripts/tile_manager.gd
@@ -5,22 +5,43 @@ const tile = preload("res://scenes/Tile.tscn")
 
 export var distance = 128
 export var tile_size = 64
+export var back_distance = 64
+
 
 var player
 var pos_z = 0
 
+var tiles
+var numTiles = 0
 
 func _ready() -> void:
 	player = get_node("/root/Game/Player")
+	tiles = []
 
 
 func _process(_delta: float) -> void:
 	if player.translation.z - distance < pos_z:
 		add_tile()
+	if numTiles>0:
+		var firstTile = tiles[0] as Spatial
+		print("SEVERAL TILES")
+		if firstTile:
+			print("NOT NIL")
+			if firstTile.translation.z>player.translation.z+back_distance: # player goes towards -z
+				print("DELETING A TILE")
+				firstTile.queue_free()
+				numTiles-=1
+				tiles.pop_front()
+
+
+		
+	
 
 
 func add_tile():
 	var new = tile.instance()
+	tiles.append(new as Spatial)
+	numTiles+=1
 	pos_z -= tile_size
 	new.translation.z = pos_z
 	add_child(new)
