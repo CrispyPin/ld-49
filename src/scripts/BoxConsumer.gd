87e9a6419d335c4e035fb5049ffdeb65b5d38ad6
ErikBot42 <erik.arn.magnusson@gmail.com>
2021-10-03 21:11:08 +0200

    truck, box handling

--- src/scripts/BoxConsumer.gd
+++ src/scripts/BoxConsumer.gd
@@ -2,8 +2,9 @@ extends Area
 
 
 func _ready():
-    monitoring = true
+	monitoring = true
 
 func _on_BoxConsumer_body_entered(body):
+	print("AAAAAAAAAA")
 	if body.is_in_group("box"):
 		body.queue_free()
