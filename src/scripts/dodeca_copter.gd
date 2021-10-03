1df41fd736dd4c33806c84700f9c00f93ce1fbf0
ErikBot42 <erik.arn.magnusson@gmail.com>
2021-10-03 23:13:08 +0200

    dodecacopter harder

--- src/scripts/dodeca_copter.gd
+++ src/scripts/dodeca_copter.gd
@@ -8,12 +8,25 @@ var attack := false
 func _ready() -> void:
 	pass
 
-var playerSpeedFactor := 3
+var maxSpeed = 20
+var forceFactor = 5
+var playerSpeedFactor: float = 0.003*forceFactor
+
+
 
 func _physics_process(delta: float) -> void:
-	var target : Vector3 = player.global_transform.origin#+linear_velocity*playerSpeedFactor
-	var dir : Vector3 = (target - global_transform.origin).normalized()
-	add_central_force(5*dir)
+	var distToPlayer = player.global_transform.origin.distance_to(global_transform.origin)
+	var target: Vector3 = player.global_transform.origin+player.linear_velocity*playerSpeedFactor*distToPlayer
+	var targetDir: Vector3 = (target - global_transform.origin).normalized()
+	var error: Vector3 = linear_velocity-linear_velocity.project(targetDir)
+	if error.length() > 3:
+		error = error.normalized()*3
+
+	var total: Vector3 = -error + targetDir
+
+	add_central_force(forceFactor*total)
+	if (linear_velocity.length()>maxSpeed):
+		add_central_force(-linear_velocity.normalized()*10)
 	#	if attack:
 	#		linear_velocity = (target - global_transform.origin).normalized() * 8
 	#		if (translation - target).length() < 8:
