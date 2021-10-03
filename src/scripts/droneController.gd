123ab517a51149c85b26b4956b9535b88f0bfa34
ErikBot42 <erik.arn.magnusson@gmail.com>
2021-10-03 14:34:15 +0200

    Fixed player controller

--- src/scripts/droneController.gd
+++ src/scripts/droneController.gd
@@ -9,7 +9,13 @@ extends RigidBody
 # Called when the node enters the scene tree for the first time.
 var gravity = 9.8
 var yAcceleration = gravity
-var throttleSpeed = 0.2
+var throttleSpeed = 0.2*60
+
+var ySpeedDamping = 0.05
+var directYcontrolFactor = 5
+
+var maxLateralSpeed = 50
+
 func _ready():
 
 
@@ -20,15 +26,21 @@ var tiltSpeed = 2
 var xdir = 0; # left right
 var zdir = 0; # forward backward
 
+var ydir = 0; # up down
+
 func _integrate_forces(state):
 	#state.set_angular_velocity(Vector3(0,yAcceleration,0))
-	state.transform = transform.interpolate_with(transform.looking_at(translation + Vector3(0,zdir,-1),Vector3(xdir,1,zdir)),state.step*tiltSpeed)
+	var xdirReduced = xdir-(linear_velocity.x/maxLateralSpeed)
+	var zdirReduced = zdir-(linear_velocity.z/maxLateralSpeed)
+	state.transform = transform.interpolate_with(transform.looking_at(translation + Vector3(0,zdirReduced,-1),Vector3(xdirReduced,1,zdirReduced)),state.step*tiltSpeed)
 	pass
 
 func _physics_process(_delta):
 	#look_at(translation + Vector3(0,zdir,-1),Vector3(xdir,1,zdir))
 	var decreaseAccelerationFactor = translation.y*0.7
 	var totalYAcceleration = (yAcceleration-decreaseAccelerationFactor)
+	totalYAcceleration -= pow(linear_velocity.y,3)*ySpeedDamping
+	totalYAcceleration += ydir*directYcontrolFactor
 
 	var up = transform.basis.y
 
@@ -52,11 +64,15 @@ func _process(delta):
 		xdir = -1.0
 	elif Input.is_action_pressed("right"):
 		xdir = 1.0
-
+	
+	ydir = 0
 	if Input.is_action_pressed("decrease_throttle"):
-		yAcceleration-=throttleSpeed
+		ydir = -1.0;
 	if Input.is_action_pressed("increase_throttle"):
-		yAcceleration+=throttleSpeed
+		ydir = 1.0
+
+
+	yAcceleration+=ydir*throttleSpeed*delta
 
 	yAcceleration = max(yAcceleration, gravity*1.05)
 	$drone.rot_speed = (yAcceleration/gravity)*15
