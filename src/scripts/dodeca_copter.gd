c8b581bc922f276c834a93afa45163531df089f1
ErikBot42 <erik.arn.magnusson@gmail.com>
2021-10-03 22:07:23 +0200

    dodecacopter work

--- src/scripts/dodeca_copter.gd
+++ src/scripts/dodeca_copter.gd
@@ -8,9 +8,11 @@ var attack := false
 func _ready() -> void:
 	pass
 
+var playerSpeedFactor := 3
 
 func _physics_process(delta: float) -> void:
-	var dir : Vector3 = (player.global_transform.origin - global_transform.origin).normalized()
+	var target : Vector3 = player.global_transform.origin#+linear_velocity*playerSpeedFactor
+	var dir : Vector3 = (target - global_transform.origin).normalized()
 	add_central_force(5*dir)
 	#	if attack:
 	#		linear_velocity = (target - global_transform.origin).normalized() * 8
