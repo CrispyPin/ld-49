0db0f962d4782ed1ea9ba53c814e402ef233b263
ErikBot42 <erik.arn.magnusson@gmail.com>
2021-10-04 11:55:24 +0200

    truck acceleratoin

--- src/scripts/Truck.gd
+++ src/scripts/Truck.gd
@@ -1,12 +1,19 @@
 extends VehicleBody
 
-
+var enableEngine := false
+var currentForce := 0
 func _ready() -> void:
 	#engine_force = 1*mass
 	$Deleter.connect("box_consumed", self, "_on_Deleter_box_consumed")
 	pass
 
+func _process(delta):
+	if enableEngine:
+		currentForce += delta*mass*0.3
+		engine_force = currentForce
+	else:
+		engine_force = 0
 
 func _on_Deleter_box_consumed() -> void:
 	print("DELETE SIGNAL RECIVED")
-	engine_force = 1*mass
+	enableEngine = true
